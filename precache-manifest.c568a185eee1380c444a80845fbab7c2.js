d9136c5cf7251dbd90280b330b3dfeef33085120
Singhey <street17cafe@gmail.com>
2020-07-11 20:25:04 +0530

    Admin ui

---
+++ precache-manifest.c568a185eee1380c444a80845fbab7c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5f46da8642583a4aa1adf595a5f840",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "6a9ec8210ac9bf3dd14c",
+    "url": "/admin/static/js/2.4ca18cc6.chunk.js"
+  },
+  {
+    "revision": "f3062b8eb574be72694c8c5cf0746c25",
+    "url": "/admin/static/js/2.4ca18cc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be7bb5643ab557207363",
+    "url": "/admin/static/js/main.fb30df16.chunk.js"
+  },
+  {
+    "revision": "5f41cc2c5b69e7d897fe",
+    "url": "/admin/static/js/runtime-main.c28f04d1.js"
+  }
+]);
