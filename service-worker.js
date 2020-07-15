49573789d8fc5ceb7bc21c32bdc5215de62f7fde
Singhey <street17cafe@gmail.com>
2020-07-16 02:46:12 +0530

    Added charts and overview

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/admin/precache-manifest.982c3e76d5cad11eaf7269790b7e8089.js"
+  "/admin/precache-manifest.a8cbcb85299f3ea0431f77ca69fb01ae.js"
 );
 
 self.addEventListener('message', (event) => {
